e00080f05b93e36bb745784b9b2a5994075c8031
Hubert Fijołek <hubertfijolek1@gmail.com>
2023-03-30 21:40:41 +0200

    Adjusting view of CarPartDetails component

--- frontend/src/components/CarPartDetails.vue
+++ frontend/src/components/CarPartDetails.vue
@@ -1,14 +1,38 @@
 <template>
   <div class="car-part-details">
     <h1>{{ carPart.title }}</h1>
-    <p>Brand: {{ carPart.brand }}</p>
-    <p>Model: {{ carPart.model }}</p>
-    <p>Year: {{ carPart.year_from }}</p>
-    <p>Price: {{ carPart.price }}</p>
-    <p>Part_number: {{ carPart.part_number }}</p>
-    <p>Color_model: {{ carPart.color_model }}</p>
-    <p>Condition: {{ carPart.condition }}</p>
-    <p>Description: {{ carPart.description }}</p>
+    <div class="main-info">
+      <div class="info-row">
+        <div class="info-cell">Brand: {{ carPart.brand }}</div>
+        <div class="info-cell">Model: {{ carPart.model }}</div>
+      </div>
+      <div class="info-row">
+        <div class="info-cell">Year: {{ carPart.year_from }}</div>
+        <div class="info-cell">Price: {{ carPart.price }} PLN</div>
+      </div>
+    </div>
+    <div class="details-container">
+      <div class="part-details">
+        <h1>Additional informations about part</h1>
+        <div class="details-info">
+          <div class="details-row">
+            <div class="details-cell">Part_number: {{ carPart.part_number }}</div>
+            <div class="details-cell">Color_model: {{ carPart.color_model }}</div>
+          </div>
+          <div class="details-row">
+            <div class="details-cell">Condition: {{ carPart.condition }}</div>
+            <div class="details-cell">Seller: {{ user.username }}</div>
+          </div>
+        </div>
+        <h1>Description</h1>
+        <p>{{ carPart.description }}</p>
+      </div>
+      <div class="seller-info">
+        <h3>Informations about seller:</h3>
+        <p>Username: {{ user.username }}</p>
+        <p>Email: {{ user.email }}</p>
+      </div>
+    </div>
     <button @click="goToHomePage">Back to homepage</button>
   </div>
 </template>
@@ -25,17 +49,24 @@ export default {
   },
   data() {
     return {
-      carPart: {}
+      carPart: {},
+      user: {} // Dodane
     }
   },
   mounted() {
     axios.get(`/api/car_parts/${this.id}/`)
       .then(response => {
         this.carPart = response.data;
+        // Dodane: Pobierz dane użytkownika po otrzymaniu carPart
+        return axios.get(`/api/users/${this.carPart.seller}/`);
+      })
+      .then(response => {
+        // Dodane: Zaktualizuj dane użytkownika
+        this.user = response.data;
       })
       .catch(error => {
         console.log(error);
-      })
+      });
   },
   methods: {
     goToHomePage() {
@@ -44,7 +75,6 @@ export default {
   }  
 }
 </script>
-
 <style>
 .car-part-details {
   display: flex;
@@ -52,23 +82,100 @@ export default {
   align-items: center;
   flex-direction: column;
   margin-top: 20px;
-  font-size: 16px;
-  color: white;
-  background-color: #333;
-  border-radius: 10px;
-  padding: 20px;
-  box-shadow: 0px 0px 10px #333;
+  font-size: 20px;
+  color: black;
+  width: 95%; /* Dodane */
+  margin-left: auto;
+  margin-right: auto;
+  /* background-color: #f0e1c1; */
 }
 
 .car-part-details h1 {
-  font-size: 24px;
+  font-size: 32px;
   margin-bottom: 10px;
 }
 
-.car-part-details p {
-  margin: 5px;
+.main-info {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 15px;
+  background-color: #2432c7;
+  padding: 15px;
+  border-radius: 5px;
+  width: 100%;
+  color:white
 }
- 
+
+.info-row {
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+}
+
+.info-cell {
+  width: 50%;
+  text-align: center;
+}
+
+.details-container {
+  display: flex;
+  flex-direction: column; /* Zaktualizowane */
+  width: 100%; /* Zaktualizowane */
+}
+
+.part-details {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  padding: 15px;
+  border-radius: 5px;
+  width: 100%;
+  margin-left: auto;
+  margin-right: auto;
+}
+.part-details h1 {
+  font-size: 30px;
+  margin-bottom: 10px;
+}
+.part-details p {
+  /* font-size: 30px; */
+  text-align: center;
+  margin-bottom: 5px;
+}
+
+.details-info {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  background-color: #2432c7;
+  color: white;
+  padding: 15px;
+  margin-bottom: 20px;
+}
+
+.details-row {
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+}
+
+.details-cell {
+  width: 50%;
+  text-align: center;
+}
+
+.seller-info {
+  text-align: center; /* Zaktualizowane */
+  padding: 15px;
+  border-radius: 5px;
+  width: 100%; /* Dodane */
+  margin-top: 20px; /* Dodane */
+  
+}
+
 .car-part-details button {
   margin-top: 20px;
   padding: 10px 20px;
